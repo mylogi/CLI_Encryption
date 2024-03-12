c574e32498f435a8024a8f687911b5a7f85e0be5
mylogi <mp.gplay@gmail.com>
2024-03-12 14:33:59 +0200

    ✨ Add the main function, refactor structure.

--- encytool.py
+++ encytool.py
@@ -1,8 +1,13 @@
 from classes.command_line_tool import CommandLineTool
 
-if __name__ == "__main__":
+
+def main() -> None:
     tool = CommandLineTool()
     state_tuple_data = tool.run()
     tool.input_loop()
     print(f"{tool.input_text=}")
     print(f"{tool.encrypt_or_decrypt(state_tuple_data)=}")
+
+
+if __name__ == "__main__":
+    main()
